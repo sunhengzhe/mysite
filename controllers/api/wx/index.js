e3392b64fa960d4df1c690faa69e8c069a5a7cd4
sunhengzhe <825980764@qq.com>
2017-10-25 23:28:17 +0800

    添加回复接口

--- controllers/api/wx/index.js
+++ controllers/api/wx/index.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const crypto = require('crypto');
+const logger = require('winston');
 const config = require('./config');
+const { parseString, Builder } = require('xml2js');
+const xmlBuilder = new Builder();
 
 const router = express.Router();
-const sha1 = crypto.createHash('sha1');
+
 
 /**
  * 认证
  */
 router.get('/handle', (req, res, next) => {
     const {
-        signature,
-        timestamp,
-        nonce,
-        echostr,
+        signature = '499172f8974aaa9cd87fd766ae54e36a5443017b',
+        timestamp = '2017-10-25 22:56:08',
+        nonce = '180802211',
+        echostr = '16751326018643340984',
     } = req.query;
 
     const token = config.token;
@@ -21,6 +24,8 @@ router.get('/handle', (req, res, next) => {
     const list = [token, timestamp, nonce];
 
     const str = list.sort().join('');
+
+    const sha1 = crypto.createHash('sha1');
 
     sha1.update(str);
 
@@ -30,7 +35,36 @@ router.get('/handle', (req, res, next) => {
         return res.send(echostr);
     }
 
-    return res.send('I am handle');
+    return res.send(str);
+});
+
+/**
+ * 获取消息
+ */
+router.post('/handle', (req, res, next) => {
+    const msg = req.body;
+
+    parseString(msg, function (err, result) {
+        if (err) {
+            logger.error('api.wx.post.handle', err);
+            return res.send('success');
+        }
+
+        const { ToUserName, FromUserName, Content } = result;
+
+        const reply = {
+            ToUserName: FromUserName,
+            FromUserName: ToUserName,
+            CreateTime: +new Date(),
+            MsgType: 'text',
+            Content: `
+                欢迎
+                you are my secret
+            `
+        };
+
+        return res.send(xmlBuilder.buildObject(reply));
+    });
 });
 
 module.exports = router;
